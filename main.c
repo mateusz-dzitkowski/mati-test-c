b05b0dc0f1139cfa7342ab7436d2fabe0e96b12a
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-06-23 19:48:13 +0200

    counter of digits

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 
 int main() {
     int c;
+    long whitespace = 0;
+    long digits[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    long others = 0;
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\t') {
-            putchar('\n');
+        if (c >= '0' && c <= '9') {
+            digits[c - '0']++;
+        } else if (c == ' ' || c == '\t' || c == '\n') {
+            whitespace++;
         } else {
-            putchar(c);
+            others++;
         }
     }
+
+    printf("whitespace: %ld\n", whitespace);
+    for (int i = 0; i <= 9; i++) {
+        printf("digit %d: %ld\n", i, digits[i]);
+    }
+    printf("others: %ld\n", others);
 }
