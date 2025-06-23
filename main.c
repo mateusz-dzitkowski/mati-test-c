1b6fa21c2c0b218d928a41e778bc7c58e5db9afe
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-06-23 19:36:46 +0200

    poor mans wc

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int c;
 
+    bool in_word = false;
+    long lines = 0;
+    long words = 0;
+    long chars = 0;
+
     while ((c = getchar()) != EOF) {
-        switch (c) {
-            case '\t': putchar('\\'); putchar('t'); break;
-            case '\b': putchar('\\'); putchar('b'); break;
-            case '\\': putchar('\\'); putchar('\\'); break;
-            default: putchar(c); break;
+        chars++;
+
+        if (c == '\n') {
+            lines++;
+        }
+        if (c == ' ' || c == '\t' || c == '\n') {
+            in_word = false;
+        } else if (!in_word) {
+            in_word = true;
+            words++;
         }
     }
+
+    printf("\t%ld\t%ld\t%ld\n", lines, words, chars);
 }
