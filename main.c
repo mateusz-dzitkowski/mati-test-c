0be6677d6b9a32e1323c9e88faafe12699d88b87
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-06-23 19:38:29 +0200

    exercise 1-12

--- main.c
+++ main.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
     int c;
 
-    bool in_word = false;
-    long lines = 0;
-    long words = 0;
-    long chars = 0;
-
     while ((c = getchar()) != EOF) {
-        chars++;
-
-        if (c == '\n') {
-            lines++;
-        }
-        if (c == ' ' || c == '\t' || c == '\n') {
-            in_word = false;
-        } else if (!in_word) {
-            in_word = true;
-            words++;
+        if (c == ' ' || c == '\t') {
+            putchar('\n');
+        } else {
+            putchar(c);
         }
     }
-
-    printf("\t%ld\t%ld\t%ld\n", lines, words, chars);
 }
