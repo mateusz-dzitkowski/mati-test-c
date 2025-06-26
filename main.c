9d412ff896e352fc0bfa6747d40330a8c1fb39ff
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-06-26 21:12:35 +0200

    exercise 2-6

--- main.c
+++ main.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 
-bool contains(const char str[], const char c) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == c) {
-            return true;
-        }
-    }
-    return false;
-}
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+ ((byte) & 0x80 ? '1' : '0'), \
+ ((byte) & 0x40 ? '1' : '0'), \
+ ((byte) & 0x20 ? '1' : '0'), \
+ ((byte) & 0x10 ? '1' : '0'), \
+ ((byte) & 0x08 ? '1' : '0'), \
+ ((byte) & 0x04 ? '1' : '0'), \
+ ((byte) & 0x02 ? '1' : '0'), \
+ ((byte) & 0x01 ? '1' : '0')
+#define PRINT_BITS(x) printf(BYTE_TO_BINARY_PATTERN "\n", BYTE_TO_BINARY(x))
 
-char* any(char s1[], char s2[]) {
-    for (int i = 0; s1[i] != '\0'; i++) {
-        if (contains(s2, s1[i])) {
-            return &s1[i];
-        }
-    }
-    return NULL;
+unsigned int set_bits(const unsigned int x, const int p, const int n, const unsigned int y) {
+    const int shift = p - n + 1;
+    const int mask_static = (1 << n) - 1;
+    const int mask = mask_static << shift;
+    return x & ~mask | (y & mask_static) << shift;
 }
 
 int main() {
-    char word[] = "hello, world!";
+    const unsigned int x = 992;
+    const unsigned int y = 5;
+    const unsigned int z = set_bits(x, 4, 3, y);
 
-    char* out = any(word, "rod");
-    printf("%s\n", out);
-
-    char* out_std = strpbrk(word, "rod");
-    printf("%s\n", out_std);
-
-    char* null = any(word, "g");
-    printf("%s\n", null);
+    PRINT_BITS(z);
 
     return 0;
 }
