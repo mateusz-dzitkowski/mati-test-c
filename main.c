bb53525c6f676c4ece92756b3aef9e97ee42b5d6
mateusz.dzitkowski <mateusz.dzitkowski@irdeto.com>
2025-06-26 20:38:11 +0200

    exercise 2-4

--- main.c
+++ main.c
@@ -1,59 +1,33 @@
-#include <math.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
 
-char* inet_ntoa(const unsigned int ip) {
-    const unsigned char octet_1 = ip >> 24 & 255;
-    const unsigned char octet_2 = ip >> 16 & 255;
-    const unsigned char octet_3 = ip >> 8 & 255;
-    const unsigned char octet_4 = ip & 255;
-
-    char *ret = malloc(15*sizeof(char));
-    if (ret == NULL) {
-        return NULL;
+bool contains(const char str[], const char c) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == c) {
+            return true;
+        }
     }
-
-    sprintf(ret, "%d.%d.%d.%d",  octet_1, octet_2, octet_3, octet_4);
-    return ret;
+    return false;
 }
 
-unsigned int inet_aton(const char *ip) {
-    if (ip == NULL) {
-        return 0;
-    }
-
-    char *ip_copy = malloc(15*sizeof(char));
-    for (int i = 0; i < 15 && ip[i] != '\0'; i++) {
-        ip_copy[i] = ip[i];
-    }
-
-    unsigned int ret = 0;
-    for (int i = 3; i >= 0; i--) {
-        const char *octet = strsep(&ip_copy, ".");
-        char *err = 0;
-        if (!octet) {
-            return 0;
+void squeeze(char s[], const char c[]) {
+    int j;
+    for (int i = j = 0; s[i] != 0; i++) {
+        if (!contains(c, s[i])) {
+            s[j] = s[i];
+            j++;
         }
-        const long n = strtol(octet, &err, 10);
-        if (*err != '\0') {
-            return 0;
-        }
-        ret += pow(256, i) * n;
     }
-
-    free(ip_copy);
-    return ret;
+    s[j] = '\0';
 }
 
 int main() {
-    const unsigned int ip_number = 123456789;
-    char *ip_string = inet_ntoa(ip_number);
-    printf("%s\n", ip_string);
+    char word[] = "hello, world!";
+    const char blacklist[] = "hl";
 
-    const unsigned int ip_number_back = inet_aton(ip_string);
-    printf("%d\n", ip_number_back);
+    squeeze(word, blacklist);
 
-    free(ip_string);
+    printf("%s", word);
+
     return 0;
 }
